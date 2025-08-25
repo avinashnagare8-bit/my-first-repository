80a9b31629730a91c0290613ee3bc1ac92c8dd34
avinashnagare8-bit <avinashnagare8@gmail.com>
2025-08-25 13:04:04 +0530

    Add files via upload

---
+++ 00 52 AWS 5.sql
@@ -0,0 +1,5 @@
+select * from information_schema.tables
+use online6
+select * from emptable
+
+select * from emp
---
+++ SSMS query.sql
@@ -0,0 +1,12 @@
+create database company;
+
+use company;
+
+CREATE TABLE emp (id INT, [product name] VARCHAR(20),[sales count] INT);
+select * from emp
+
+#actual > testing.csv file data load into emp table (company database)
+
+insert into emp values(1,'AN',100);
+insert into emp values(2,'AP',200);
+select * from emp
